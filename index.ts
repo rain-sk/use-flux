9fddbbdc4bad180672aa9dc969fd9862cbfad8f2
Spencer Rudnick <spencerudnick@gmail.com>
2018-11-14 00:18:55 -0600

    minor changes

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { Factory } from './lib/factory';
+export { Action } from './lib/interfaces';
+export { BaseState } from './lib/interfaces';
+export { Reducer } from './lib/interfaces';
--- lib/index.ts
+++
@@ -1,4 +0,0 @@
-export { Factory } from './factory';
-export { Action } from './interfaces';
-export { BaseState } from './interfaces';
-export { Reducer } from './interfaces';
