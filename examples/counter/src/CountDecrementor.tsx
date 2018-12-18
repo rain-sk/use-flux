30c502350cbab7afaf4b3dce475ff4604f797db5
Spencer Rudnick <therudnick@gmail.com>
2018-12-17 20:14:00 -0600

    clean up counter example

--- examples/counter/src/CountDecrementor.tsx
+++ examples/counter/src/CountDecrementor.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { CountContext } from './CountContext';
 
 export const CountDecrementor: React.FunctionComponent = () => {
--- examples/counter/src/CountIncrementor.tsx
+++ examples/counter/src/CountIncrementor.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import { CountContext } from './CountContext';
 
 export const CountIncrementor: React.FunctionComponent = () => {
